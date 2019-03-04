04e5687eddc1e245d24d0698b70654d35110d880
Nicolai Bonde <neobonde@gmail.com>
2019-03-04 23:25:34 +0100

    Final first pass

---
+++ Game/Assets/endSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endSound : MonoBehaviour
+{
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject.transform);
+        audioSource = GetComponent<AudioSource>();
+        transform.parent = null;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(!audioSource.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Game/Assets/spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+
+    BoxCollider2D col;
+    public GameObject elem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(elem);
+    }
+
+}
